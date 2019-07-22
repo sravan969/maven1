11939dbad98fae5a33a72f1c3378fef0798423ac
sravan969 <sravan.mghs@gmail.com>
2019-07-22 11:02:02 +0530

    1st

---
+++ src/test/java/maven1/Fibonaccitest.java
@@ -0,0 +1,17 @@
+package maven1;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class Fibonaccitest {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	
+	}
+
+}
